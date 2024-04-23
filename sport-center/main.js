a98f30e87c84ac1047b18c4e0daa9265fd189aa2
Esra Şen <sen.esra@yahoo.com>
2024-04-24 00:00:37 +0300

    Added classes page

--- sport-center/main.js
+++ sport-center/main.js
@@ -1,24 +1,91 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const navbar = document.querySelector("#navbar");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+window.addEventListener("scroll", (event) => {
+    const scroll = window.scrollY;
+    if (scroll > 60) {
+        navbar.classList.add("bg-blue");
+    }
+});
 
-setupCounter(document.querySelector('#counter'))
+const featHeader = document.querySelector("#feat-cont h2");
+const featParag = document.querySelector("#feat-cont p");
+const featImg = document.querySelector("#feat-cont img");
+const featCont = document.querySelector("#feat-cont");
+
+const yogaFunc = () => {
+    featHeader.textContent = "Yoga";
+    featParag.textContent =
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. In, nihil recusandae. Atque, illum corporis odio pariatur ad harum non, molestias maiores molestiae sapiente perferendis veniam earum fugit ipsum repellendus architecto ducimus explicabo reiciendis alias quasi culpa libero amet similique nisi!";
+    featImg.src = "./public/images/yoga.jpg";
+};
+
+const groupFunc = () => {
+    featHeader.textContent = "Group";
+    featParag.textContent =
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. In, nihil recusandae. ";
+    featImg.src = "./public/images/group.webp";
+};
+
+const soloFunc = () => {
+    featHeader.textContent = "Solo";
+    featParag.textContent =
+        "pariatur ad harum non, molestias maiores molestiae sapiente perferendis veniam earum fugit ipsum repellendus";
+    featImg.src = "./public/images/solo.jpg";
+};
+
+const stretchingFunc = () => {
+    featHeader.textContent = "Stretching";
+    featParag.textContent =
+        "pariatur ad harum non, molestias maiores molestiae sapiente perferendis veniam earum fugit ipsum repellendus";
+    featImg.src = "./public/images/stret.webp";
+};
+
+
+const btns = document.querySelector("#btnsDiv");
+
+btns.addEventListener("click", (event) => {
+    const btnId = event.target.id;
+    switch (btnId) {
+        case "yoga":
+            yogaFunc();
+            break;
+        case "group":
+            groupFunc();
+            break;
+        case "solo":
+            soloFunc();
+            break;
+        case "stretching":
+            stretchingFunc();
+            break;
+        default:
+            return;
+    }
+});
+
+const gallery = document.querySelector("#gallery");
+const galleryDiv = document.querySelector("#galleryDiv");
+const imageArr = [
+    "./public/images/yoga.jpg",
+    "./public/images/group.webp",
+    "./public/images/solo.jpg",
+    "./public/images/stret.webp",
+];
+
+let index = 0;
+
+galleryDiv.addEventListener("click", (event) => {
+    const id = event.target.id;
+    if (id === "left") {
+        index--;
+        if (index < 0) {
+            index = imageArr.length - 1;
+        }
+    } else if (id === "right") {
+        index++;
+        if (index >= imageArr.length) {
+            index = 0;
+        }
+    }
+    gallery.src = imageArr[index];
+});
